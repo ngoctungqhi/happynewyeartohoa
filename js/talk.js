2a53d7964e2af5d184a34c37a8b4c16573d64cc9
tungvn <vnt2908@gmail.com>
2022-01-31 14:57:39 +0700

    first commit

---
+++ js/talk.js
@@ -0,0 +1,68 @@
+$(function () {
+	$("#yes").click(function (event) {
+		modal("Oke, tự tin là tốt nha :))", function () {
+			$(".page_one").addClass("hide");
+			$(".page_two").removeClass("hide");
+			fireworks();
+		});
+	});
+	$("#no").click(function (event) {
+		modal("Ơ, chưa sẵn sàng ư :(", A);
+	});
+});
+
+function A() {
+	modal("Sao thế, có anh qua năm cùng mà &#128522", B);
+}
+
+function B() {
+	modal("Sao phải xoắn nhể &#128526;", C);
+}
+
+function C() {
+	modal("Năm mới rồi", D);
+}
+
+function D() {
+	modal("Anh chúc em và gia đình thật nhiều sức khoẻ &#128170", E);
+}
+
+function E() {
+	modal("Thật nhiều hạnh phúc và may mắn &#128158", F);
+}
+
+function F() {
+	modal("Chúc Dung 2022 phát tài phát lộc &#128181", G);
+}
+
+function G() {
+	modal("Tiền vô xồng xộc, tiền ra từ từ", H);
+}
+
+function H() {
+	modal("Sức khoẻ có dư, công danh tấn tới", I);
+}
+
+function I() {
+	modal("Tình duyên phơi phới, hạnh phúc thăng hoa", J);
+}
+
+function J() {
+	modal("Làm điều mình thích và thích điều mình làm nha", function () {
+		fireworks();
+	});
+}
+
+function fireworks() {
+	$(".page_one").addClass("hide");
+	initAnimate();
+}
+
+function modal(content, callback) {
+	var tpl = '<div class="container">' + '<div class="mask"></div>' + '<div class="modal">' + "<p>" + content + "</p>" + '<button type="button" id="confirm" class="confirm">Tiếp</button>' + "</div>" + "</div>";
+	$("body").append(tpl);
+	$(document).on("click", ".confirm", function () {
+		$(".container").remove();
+		callback();
+	});
+}
